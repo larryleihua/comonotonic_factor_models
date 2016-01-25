0962d5de3bb10ec1a14c74b716451f85f9dcda95
larryleihua <larry.lei.hua@gmail.com>
2016-01-24 18:40:05 -0600

    header file

---
+++ comFac.hpp
@@ -0,0 +1,17 @@
+#ifndef COMFAC_HPP
+#define COMFAC_HPP
+
+/*---------------------------------------------*/
+/* Comonotonic factor models                   */
+/* Lei Hua, Jan, 2016                          */
+/*---------------------------------------------*/
+
+
+#include <vector>
+using std::vector;
+
+void gauleg(int, vector<double>&, vector<double>&);
+
+
+#endif /* COMFAC_HPP */
+
